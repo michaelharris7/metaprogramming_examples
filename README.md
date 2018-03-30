ae5a3effce7a7674a3f65f89b441bbaf7da9319e
Michael Harris <michaeljh7@gmail.com>
2018-03-30 16:22:00 -0600

    Added metaprogramming examples I created

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # metaprogramming_examples
+
+This is a compilation of some metaprogramming examples I have created.
---
+++ metaprogramming_example_1.rb
@@ -0,0 +1,20 @@
+class Baseball
+end
+
+# p Baseball.new.swing
+
+class Baseball
+  def swing
+    "homerun"
+  end
+end
+
+# p Baseball.new.swing
+
+class Baseball
+  def swing
+    "Strike"
+  end
+end
+
+p Baseball.new.swing
